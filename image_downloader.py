57eb83c7c1c638a959e18aece7dc43d88dded519
barcol <barcol1@interia.pl>
2019-03-01 22:06:04 +0100

    exception handling provided

--- image_downloader.py
+++ image_downloader.py
@@ -1,6 +1,7 @@
 """This image downloader only works on text files that contains exactly one .jpg link in every line"""
 import os
 import urllib.request
+import urllib.error
 
 from tqdm import tqdm
 
@@ -9,13 +10,22 @@ class ImageDownloader:
     def __init__(self):
         pass
 
-    def download(self, images: str):
+    def download(self, images: str, path: str):
         newpath = f"./{self.__file_to_folder_name(images)}"
         if not os.path.exists(newpath):
             os.makedirs(newpath)
-        with open("imagesToDownload/" + images, "r") as file:
-            for number, line in (tqdm(enumerate(file))):
-                urllib.request.urlretrieve(line, f"{self.__file_to_folder_name(images)}/{number}.jpg")
+        with open(path + "/" + images, "r") as file:
+            for number, link in (tqdm(enumerate(file))):
+                try:
+                    urllib.request.urlretrieve(link, f"{self.__file_to_folder_name(images)}/{number}.jpg")
+                except urllib.error.HTTPError:
+                    print(f"\n{link} does not contain any image!")
+                except urllib.error.URLError:
+                    print(f"\n{link} is not a valid image link!")
+                except ValueError:
+                    print("Invalid link")
+                except TimeoutError:
+                    print("Connection time out")
 
     @staticmethod
     def __file_to_folder_name(name):
--- main.py
+++ main.py
@@ -4,11 +4,16 @@ from image_downloader import ImageDownloader as Id
 
 
 def run():
+    download_images()
+
+
+def download_images():
+    """files have to be stored in imagesToDownload directory"""
     downloader = Id()
-    for file in os.listdir("imagesToDownload"):
-        if file.endswith(".txt"):
-            print(os.path.join("/mydir", file))
-            downloader.download("testfile.txt")
+    downloader.download("beer_in_glasses.txt", "imagesToDownload")
+    #for file in os.listdir("imagesToDownload"):
+    #    if file.endswith(".txt"):
+    #        downloader.download(file, "imagesToDownload")
 
 
 if __name__ == "__main__":
