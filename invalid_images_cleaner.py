b07bdc101a4aa18b6ee45b3e62e873a801504712
barcol <barcol1@interia.pl>
2019-03-06 17:33:40 +0100

    deleting unnecessary images based on examples and empty files

--- invalid_images_cleaner.py
+++ invalid_images_cleaner.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import numpy as np
 
 
 class InvalidImagesCleaner:
@@ -9,13 +10,17 @@ class InvalidImagesCleaner:
     def iterate_folders(self, mold_directory):
         for file in os.listdir(mold_directory):
             if file.endswith(".jpg"):
-                delete_mold = cv2.imread(file, 0)
+                delete_mold = cv2.imread(mold_directory + "/" + file, 0)
                 self.delete_useless_images("beer_in_bottles", delete_mold)
                 self.delete_useless_images("beer_in_glasses", delete_mold)
 
     @staticmethod
-    def delete_useless_images(path, image_mold):
+    def delete_useless_images(path: str, image_mold):
         for file in os.listdir(path):
-            image = cv2.imread(file, 0)
-            if image == image_mold:
-                os.remove(file)
+            image = cv2.imread(path + "/" + file, 0)
+            try:
+                if image.shape == image_mold.shape and not(np.bitwise_xor(image, image_mold).any()):
+                    pass
+                    # os.remove(path + "/" + file)
+            except AttributeError:
+                os.remove(path + "/" + file)
